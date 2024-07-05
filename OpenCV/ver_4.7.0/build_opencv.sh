b1d5d395a4e5a7eb0d2d182616982a474bc6685d
13900k <tiaf_py@163.com>
2024-07-05 08:02:53 +0800

    update opencv build.sh

--- OpenCV/ver_4.7.0/build_opencv.sh
+++ OpenCV/ver_4.7.0/build_opencv.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 set -ex
-# ------------ set path ---------------
+# ------------ set opensource path and install path ---------------
 CURRENT_DIR=$(cd $(dirname $0); pwd)
 YASM_DIR=${CURRENT_DIR}/yasm-1.3.0
-FFMPEG_DIR=${CURRENT_DIR}/ffmpeg-4.4.4
+FFMPEG_DIR=${CURRENT_DIR}/ffmpeg-5.1.5
 OPENCV_DIR=${CURRENT_DIR}/opencv-4.7.0
+OPEN_SOURCE_DIR=${CURRENT_DIR}/open_source
+if [ ! -d ${OPEN_SOURCE_DIR} ];then
+  mkdir ${OPEN_SOURCE_DIR}
+fi
 # ------------ build yasm and install ---------------
- cd ${YASM_DIR}
- ./configure --prefix=${YASM_DIR}/install
- make -j$(nproc) && make install
- export PATH=$PATH:${YASM_DIR}/install/bin
+YASM_INSTALL_DIR=${OPEN_SOURCE_DIR}/yasm
+if [ -d ${YASM_INSTALL_DIR} ];then
+  rm -rf ${YASM_INSTALL_DIR}
+fi
+mkdir ${YASM_INSTALL_DIR}
+
+cd ${YASM_DIR}
+./configure --prefix=${YASM_INSTALL_DIR}
+make -j$(nproc) && make install
+
+export PATH=$PATH:${YASM_INSTALL_DIR}/bin
 # ------------ build ffmpeg and install ---------------
- cd ${FFMPEG_DIR}
- ./configure --disable-static --enable-shared --prefix=install
- make -j$(nproc) && make install
+FFMPEG_INSTALL_DIR=${OPEN_SOURCE_DIR}/ffmpeg
+if [ -d ${FFMPEG_INSTALL_DIR} ];then
+  rm -rf ${FFMPEG_INSTALL_DIR}
+fi
+mkdir ${FFMPEG_INSTALL_DIR}
+
+cd ${FFMPEG_DIR}
+./configure --disable-static --enable-shared --disable-swresample --prefix=${FFMPEG_INSTALL_DIR}
+make -j$(nproc) && make install
 # ------------ build opencv and install ---------------
+OPENCV_INSTALL_DIR=${OPEN_SOURCE_DIR}/opencv
+if [ -d ${OPENCV_INSTALL_DIR} ];then
+  rm -rf ${OPENCV_INSTALL_DIR}
+fi
+mkdir ${OPENCV_INSTALL_DIR}
+
 cd ${OPENCV_DIR}
 # 指定ffmpeg自编译版本
-export LD_LIBRARY_PATH=/home/sgt/Third/ffmpeg/lib/:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=/home/sgt/Third/ffmpeg/lib/pkgconfig/:$PKG_CONFIG_PATH
-export PKG_CONFIG_LIBDIR=/home/sgt/Third/ffmpeg/lib/:$PKG_CONFIG_LIBDIR
+export LD_LIBRARY_PATH=${FFMPEG_INSTALL_DIR}/lib/:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=${FFMPEG_INSTALL_DIR}/lib/pkgconfig/:$PKG_CONFIG_PATH
+export PKG_CONFIG_LIBDIR=${FFMPEG_INSTALL_DIR}/lib/:$PKG_CONFIG_LIBDIR
 
-rm -rf ./build_demo ./install_demo
-mkdir build_demo install_demo && cd build_demo
+rm -rf ./build && mkdir build && cd build
 cmake .. \
 -D WITH_GTK=OFF \
 -D CMAKE_BUILD_TYPE=RELEASE \
@@ -41,6 +63,6 @@ cmake .. \
 -D BUILD_opencv_apps=OFF \
 -D BUILD_IPP_IW=OFF \
 -D BUILD_opencv_ts=OFF \
--D CMAKE_INSTALL_PREFIX=${OPENCV_DIR}/install_demo
+-D CMAKE_INSTALL_PREFIX=${OPENCV_INSTALL_DIR}
 
- make -j$(nproc) && make install
+make -j$(nproc) && make install
